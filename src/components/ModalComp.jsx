95e9bad17d2ab61877a28dda0641f31f2238d17f
Sandro Livino <sandro.livino@gmail.com>
2023-05-26 15:12:20 -0300

    feat: modal de edição de clientes em App

--- src/components/ModalComp.jsx
+++ src/components/ModalComp.jsx
@@ -1,58 +1,61 @@
 import {
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton,
-    Button,
-    FormControl,
-    FormLabel,
-    Input,
-    Box,
-    ModalHeader,
-  } from "@chakra-ui/react";
-  import { useState } from "react";
-  
-  const ModalComp = ({ data, setData, dataEdit, isOpen, onClose }) => {
-    const [name, setName] = useState(dataEdit.name || "");
-    const [email, setEmail] = useState(dataEdit.email || "");
-  
-    const handleSave = () => {
-      if (!name || !email) return;
-  
-      if (emailAlreadyExists()){
-          return alert("E-mail já cadastrado");
-      }
-  
-      if (Object.keys(dataEdit).length) {
-          data[dataEdit.index] = {name, email};
-      }
-  
-      const newDataArray = !Object.keys(dataEdit).length
-      ? [...(data ? data : [], {name, email})]
-      : [...(data ? data: [])];
-  
-      localStorage.setItem("cad_cliente", JSON.stringify(newDataArray));
-  
-      setData(newDataArray);
-  
-      onClose();
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Box,
+} from "@chakra-ui/react";
+import { useState } from "react";
+
+const ModalComp = ({ data, setData, dataEdit, isOpen, onClose }) => {
+  const [name, setName] = useState(dataEdit.name || "");
+  const [email, setEmail] = useState(dataEdit.email || "");
+
+  const handleSave = () => {
+    if (!name || !email) return;
+
+    if (emailAlreadyExists()) {
+      return alert("E-mail já cadastrado!");
     }
-  
-    const emailAlreadyExists = () => {
-      if (dataEdit.email !== email && data?.length) {
-          return data.find((item) => item.email === email);
-      }
+
+    if (Object.keys(dataEdit).length) {
+      data[dataEdit.index] = { name, email };
     }
-  
-    return (
-      <>
-        <Modal isOpen={isOpen} onClose={onClose}>
-          <ModalOverlay />
-          <ModalContent>
-            <ModalHeader>Cadastro de Clientes</ModalHeader>
-            <ModalCloseButton />
+
+    const newDataArray = !Object.keys(dataEdit).length
+      ? [...(data ? data : []), { name, email }]
+      : [...(data ? data : [])];
+
+    localStorage.setItem("cad_cliente", JSON.stringify(newDataArray));
+
+    setData(newDataArray);
+
+    onClose();
+  };
+
+  const emailAlreadyExists = () => {
+    if (dataEdit.email !== email && data?.length) {
+      return data.find((item) => item.email === email);
+    }
+
+    return false;
+  };
+
+  return (
+    <>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Cadastro de Clientes</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
             <FormControl display="flex" flexDir="column" gap={4}>
               <Box>
                 <FormLabel>Nome</FormLabel>
@@ -63,7 +66,7 @@ import {
                 />
               </Box>
               <Box>
-                <FormLabel>Email</FormLabel>
+                <FormLabel>E-mail</FormLabel>
                 <Input
                   type="email"
                   value={email}
@@ -71,19 +74,20 @@ import {
                 />
               </Box>
             </FormControl>
-            <ModalFooter justifyContent="start">
-              <Button colorScheme="green" mr={3} onClick={handleSave}>
-                SÓ JESUS SALVA!
-              </Button>
-              <Button colorScheme="red" onClick={onClose}>
-                CANCELAR
-              </Button>
-            </ModalFooter>
-          </ModalContent>
-        </Modal>
-      </>
-    );
-  };
-  
-  export default ModalComp;
-  
+          </ModalBody>
+
+          <ModalFooter justifyContent="start">
+            <Button colorScheme="green" mr={3} onClick={handleSave}>
+              SALVAR
+            </Button>
+            <Button colorScheme="red" onClick={onClose}>
+              CANCELAR
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default ModalComp;
